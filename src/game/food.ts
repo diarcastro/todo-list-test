4848ef1b0fe4bf5d0657836e05d934ffbfe5315d
Diego Castro <ing.diegocastro@gmail.com>
2020-02-16 13:49:25 -0500

    Food refactor

--- src/game/food.ts
+++ src/game/food.ts
@@ -1,35 +1,33 @@
 import { IGame } from './interfaces'
+import { Point } from './point';
 
 const SECOND = 1000;
 
-export default class Food {
+export default class Food extends Point{
     backgroundColor: string = '#FF7F66';
     strokeColor: string = '#3F464C';
-    width: number = 16;
-    height: number = 16;
     minTime: number = 4;
     maxTime: number = 6;
     timeElapsed: number = 0;
     lastTimeOut: number = 0;
-    lastPosition: any = {};
 
     private _game: IGame;
 
     constructor (game: IGame) {
+        super();
         this._game = game;
     }
 
     randomPosition() {
-        const x = Math.floor(Math.random() * (this._game.config.width - this.width));
-        const y = Math.floor(Math.random() * (this._game.config.height - this.height));
-        return {x, y};
+        this.x = Math.floor(Math.random() * (this._game.config.width - this.width));
+        this.y = Math.floor(Math.random() * (this._game.config.height - this.height));
     }
 
     checksRegenerate() {
         const timeout = this.lastTimeOut * SECOND;
         if (this.timeElapsed * this._game.config.intervalTime >= timeout) {
             this.lastTimeOut = Math.round((Math.random() * this.maxTime)) + this.minTime;
-            this.lastPosition = this.randomPosition();
+            this.randomPosition();
             this.timeElapsed = 0;
         }
     }
@@ -39,7 +37,7 @@ export default class Food {
         this.checksRegenerate();
         const game = this._game.getContext();
         game.beginPath();
-        game.rect(this.lastPosition.x, this.lastPosition.y, this.width, this.height);
+        game.rect(this.x, this.y, this.width, this.height);
         game.fillStyle = this.backgroundColor;
         game.strokeStyle = this.strokeColor;
         game.stroke();
--- src/game/point.ts
+++ src/game/point.ts
@@ -1,6 +1,6 @@
 export class Point {
-    width: number = 16;
-    height: number = 16;
-    x: number = 0;
-    y: number = 0;
+    width?: number = 16;
+    height?: number = 16;
+    x?: number = 0;
+    y?: number = 0;
 }
